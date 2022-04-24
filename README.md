4e9a1feaa76cd26ceb46762caa0619117e8d1bff
g1tha <jeewantha.k@gmail.com>
2022-04-24 17:15:44 +1000

    Finalised project.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rock-paper-scissors
+
+Sources
+1. Rock SVG Vector (https://www.svgrepo.com/svg/92088/rock)
+2. Wrapping SVG Vector 6 (https://www.svgrepo.com/svg/155157/wrapping)
+3. Scissors 2 SVG vector (https://www.svgrepo.com/svg/1329/scissors)
+4. Man Hair SVG Vector 5 (https://www.svgrepo.com/svg/271307/man-hair)
+5. Robot SVG Vector 89 (https://www.svgrepo.com/svg/124172/robot)
--- scripts.js
+++ scripts.js
@@ -1,46 +1,58 @@
 
 const options = ["rock", "paper", "scissors"];
+const notificationPane = document.querySelector('.notifications');
+const playerScorePane = document.querySelector('.playerScore');
+const computerScorePane = document.querySelector('.computerScore');
+const playerChoice = document.getElementById('playerChoice');
+const computerChoice = document.getElementById('computerChoice');
+const roundLabel = document.querySelector('.roundLabel');
+const rounds = document.querySelector('.round');
+const playerNode = document.querySelector('.player');
+const playerBtns = playerNode.querySelectorAll('.btn');
+const restart = document.querySelector('.restart');
+
 let computerSelection;
 let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
+let roundCounter = 0;
+let maxRounds = parseInt(rounds.value);
 
-playGame();
-
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    if (playerScore > computerScore) {
-        alert(`You win the game!
-        Final score - You: ${playerScore} vs Computer: ${computerScore}`)
-    } else if (playerScore < computerScore) {
-        alert(`You lose the game!
-        Final score - You: ${playerScore} vs Computer: ${computerScore}`)
-    } else {
-        alert (`Tie game!? We paid for blooood!
-        Final score - You: ${playerScore} vs Computer: ${computerScore}`)
-    }
-}
-
+rounds.addEventListener('change', roundValue);
+playerBtns.forEach(e => e.addEventListener('click', playRound));
+restart.addEventListener('click', clearScores);
 
 function playRound() {
+    playerSelection = this.className.split(" ")[0];
+    playerChoice.innerHTML = `<img class="${playerSelection} icon" src="./img/${playerSelection}.svg" alt="${playerSelection}">${playerSelection}`;
     computerSelection = computerPlay();
-    playerSelection = null;
-    while (options.indexOf(playerSelection) < 0) {
-        playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+    computerChoice.innerHTML = `<img class="${computerSelection} icon" src="./img/${computerSelection}.svg" alt="${computerSelection}">${computerSelection}`;
+    if (roundCounter < maxRounds) {
+        if (options.indexOf(playerSelection) === 2 && options.indexOf(computerSelection) === 0) {
+            youLose();
+        } else if (options.indexOf(playerSelection) === 0 && options.indexOf(computerSelection) === 2) {
+            youWin();
+        } else if (options.indexOf(playerSelection) > options.indexOf(computerSelection)) {
+            youWin();
+        } else if (options.indexOf(playerSelection) === options.indexOf(computerSelection)) {
+            tie();
+        } else {
+            youLose();
+        } 
     }
-    if (options.indexOf(playerSelection) === 2 && options.indexOf(computerSelection) === 0) {
-        youLose();
-    } else if (options.indexOf(playerSelection) === 0 && options.indexOf(computerSelection) === 2) {
-        youWin();
-    } else if (options.indexOf(playerSelection) > options.indexOf(computerSelection)) {
-        youWin();
-    } else if (options.indexOf(playerSelection) === options.indexOf(computerSelection)) {
-        tie();
-    } else {
-        youLose();
-    } 
+    checkEnd(); 
+}
+
+function checkEnd() {
+    if (roundCounter === maxRounds) {
+        if (playerScore > computerScore) {
+            notificationPane.textContent = `You win the game!`;
+        } else if (playerScore < computerScore) {
+            notificationPane.textContent = `You lose the game!`;
+        } else {
+            notificationPane.textContent = `Tie game!? We paid for blooood!`;
+        }
+    }
 }
 
 function computerPlay() {
@@ -56,17 +68,44 @@ function computerPlay() {
 
 function youWin() {
     playerScore++;
-    alert(`You win the round, ${playerSelection} beats ${computerSelection}!
-    Current score - You: ${playerScore} vs Computer: ${computerScore}`);
+    roundCounter++;
+    notificationPane.textContent = `You win the round, ${playerSelection} beats ${computerSelection}!`;
+    updateScore();
 }
 
 function youLose() {
     computerScore++;
-    alert(`You lose the round, ${computerSelection} beats ${playerSelection}!
-    Current score - You: ${playerScore} vs Computer: ${computerScore}`);
+    roundCounter++;
+    notificationPane.textContent = `You lose the round, ${computerSelection} beats ${playerSelection}!`;
+    updateScore();
 }
 
 function tie() {
-    alert(`You tied this round. You both chose ${playerSelection}!
-    Current score - You: ${playerScore} vs Computer: ${computerScore}`)
+    roundCounter++;
+    notificationPane.textContent = `You tied this round. You both chose ${playerSelection}!`;
+    updateScore();
+}
+
+function updateScore() {
+    playerScorePane.textContent = `score: ${playerScore}`;
+    computerScorePane.textContent = `score: ${computerScore}`;
+    roundLabel.textContent = `round: ${roundCounter} of`;    
+}
+
+function clearScores() {
+    computerScore = 0;
+    playerScore = 0;
+    roundCounter = 0;
+    rounds.value = 5;
+    maxRounds = parseInt(rounds.value);
+    notificationPane.textContent = ``;
+    updateScore();
+}
+
+function roundValue() {
+    if (isNaN(rounds.value) || rounds.value < 1 || rounds.value > 9) {
+        rounds.value = 5;
+    }
+    maxRounds = parseInt(rounds.value);
+    console.log(maxRounds);
 }
